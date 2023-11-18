1d27fb3c671c7430320c5e9396b18692596c17b1
Nikita-Dev94 <nik_dev94@outlook.com>
2023-11-18 15:50:57 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express"
+const app = express()
+const port = 3001
+const a = 'Hello asdasdasdasds!'
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Разрешает доступ всем доменам
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
+
+app.get('/get', (req: any, res: any) => {
+    res.send(a)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
