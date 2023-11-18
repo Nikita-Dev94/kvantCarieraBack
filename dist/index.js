7c479975f7b29032d146369f53b83ded93321102
Nikita-Dev94 <nik_dev94@outlook.com>
2023-11-18 16:42:03 +0300

    fix_render

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+const express = require("express");
+const app = express();
+const port = 3001;
+const a = 'Hello asdasdasdasds!';
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Разрешает доступ всем доменам
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+app.get('/get', (req, res) => {
+    res.send(a);
+});
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
